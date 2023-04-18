d6ffe67bd45d273ac9a1dbf28c782aa44468c74f
GiadaPirone <123646621+GiadaPirone@users.noreply.github.com>
2023-04-18 15:26:43 +0200

    es in console

---
+++ js/main.js
@@ -0,0 +1,25 @@
+//abbiamo lista della mail che possono accedere.
+let mail = ["web.developer@gmail.com", "studente.boolean@gmail.com", "giada.pirone@gmail.com"];
+//stampiano in console le mail che possono accedere
+console.log("gli utendi che possono accedere sono:", mail);
+
+//finestra che chiede all'utente di inserire la sua mail
+let tuaMail = prompt("Inserisci qui la tua mail");
+//stampiamo in console la mail nel nuovo utente
+console.log("mail del nuovo utente:", tuaMail);
+
+for ( let c = 0; c < 3; c++) {
+
+    let mailCorrente = mail[c];
+
+    if(tuaMail == mailCorrente){
+        console.log("ma tua mail è valida");
+    } else if( tuaMail != mailCorrente){
+        console.log("la tua mail NON è valida");
+    }
+}
+
+
+
+
+
